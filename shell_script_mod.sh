8f342d1d2fb2f86c5dab62974104b547c1addf65
ylj1314 <33196759+ylj1314@users.noreply.github.com>
2021-02-24 20:03:46 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -1,6 +1,19 @@
-##下面只是做一个示例
-##使用自定义shell下载并配置执行农场		 ##使用自定义shell下载并配置执行农场
-wget -O /scripts/jd_fanslove.js https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_fanslove.js		 # wget -O /scripts/jx_nc.js https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
-echo -e >> /scripts/docker/merged_list_file.sh		 # echo -e >> /scripts/docker/merged_list_file.sh
-echo "50 19 * * * node /scripts/jd_fanslove.js |ts >> /scripts/logs/jd_fanslove.log 2>&1" >> /scripts/docker/merged_list_file.sh		 # echo "10 9,18 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jx_nc.log 2>&1" >> /scripts/docker/merged_list_file.sh
- 
+#!/bin/sh
+#i-chenzhe仓库脚本 根据iouakira 脚本复制而来
+function initShylocks() {
+ git clone https://github.com/i-chenzhe/qx.git /i-chenzhe
+  npm install
+  }
+if [ ! -d "/i-chenzhe/" ]; then
+echo "未检查到i-chenzhe仓库脚本，初始化下载相关脚本"
+ initShylocks
+ else
+  echo "更新i-chenzhe脚本相关文件"
+git -C /i-chenzhe reset --hard
+git -C /i-chenzhe pull --rebase
+npm install
+fi
+##复制两个文件
+cp -f /i-chenzhe/jd*.js /scripts/
+##使用自定义shell下载并配置执行美妆
+echo "10 8,9,10 * * * node /scripts/jd_entertainment.js |ts >> /scripts/logs/jd_entertainment.log 2>&1" >> /scripts/docker/merged_list_file.sh
